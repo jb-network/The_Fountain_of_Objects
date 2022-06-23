aad53eda6c24d876f0350e3dd290d4d4f0fff4f6
jb-network <85036487+jb-network@users.noreply.github.com>
2022-06-23 00:15:07 -0500

    Add project files.

---
+++ The_Fountain_of_Objects/Program.cs
@@ -0,0 +1,73 @@
+﻿
+
+/*
+// Future user input for expansion
+//Test Gameboard deployment
+Console.WriteLine("Enter test X...(int,int)");
+int X = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Enter test Y...(int,int)");
+int Y = Convert.ToInt32(Console.ReadLine());
+
+GameBoard board = new GameBoard(X, Y);
+Console.Write($"{board.GameMap[0,0]}");
+*/
+
+//GameBoard board = new GameBoard(3, 3);
+//Console.Write($"{board.GameMap[0,0]}");
+
+
+new GameMaster().NewGame();
+
+public class GameMaster
+{
+    public void NewGame()
+    {
+        //Test set up
+        Console.WriteLine("Please Enter your name: ");
+        string PlayerName = Console.ReadLine();
+        Player PlayerID = new Player(PlayerName);
+        GameBoard board = new GameBoard(3, 3);
+        Console.Clear();
+        //Test final set up
+        Console.WriteLine("Compass test:");
+        board.Compass();
+        //Console.Write($"\n{board.GameMap[0, 0]}");
+        Console.Write($"\nTotal Game Tiles: {board.GameMap.Length}");
+        Console.WriteLine($"\nUsername: {PlayerID.Name}");
+    }
+}
+public class GameBoard
+{
+    public int Rows { get; }
+    public int Columns { get; }
+    public readonly int[,] GameMap;
+
+    public GameBoard(int rows, int columns)
+    {
+        Rows = rows;
+        Columns = columns;
+        GameMap = new int[rows, columns];
+    }
+
+    public void Compass()
+    {        
+        Console.WriteLine("             NORTH            ");
+        Console.WriteLine("               |              ");
+        Console.WriteLine("       WEST----|----EAST      ");
+        Console.WriteLine("               |              ");
+        Console.WriteLine("             SOUTH            ");
+    }
+}
+public class Player
+{
+    public string Name { get; }
+    public int CurrentLocation { get; }
+
+    public Player(string PlayerName)
+    {
+        Name = PlayerName;
+    }
+
+}
+
+
