095cf2cf1d54ec1b569d45023d4550f1a1152abd
jb-network <85036487+jb-network@users.noreply.github.com>
2022-06-24 22:13:07 -0500

    Add files via upload

---
+++ Player.cs
@@ -0,0 +1,27 @@
+﻿using System;
+namespace The_Fountain_of_Objects
+{
+	public class Player
+	{
+		public string PlayerName { get;  }
+
+
+		public Player( string PassedName)
+		{
+			PlayerName = PassedName;
+		}
+
+		public static (int, int) SetPlayerLocation (GameBoard<string> MoveBoard, Player GamePlayer)
+        {
+			(int row, int col) PlayerLocation = (0,0);
+			MoveBoard.Map[PlayerLocation.row, PlayerLocation.col] = GamePlayer.PlayerName;
+			return PlayerLocation;
+		}
+
+		public static (int, int) UpdatePlayerLocation (Tuple tracker, Game.Direction MovePlayer, Player GamePlayer, GameBoard<string> MoveBoard)
+		{
+
+		}
+
+	}
+}
