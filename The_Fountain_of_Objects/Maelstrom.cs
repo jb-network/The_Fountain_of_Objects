a5bf96dba9c49fd0ab5b97907655de95bdec1301
jb-network <85036487+jb-network@users.noreply.github.com>
2022-06-30 11:50:41 -0500

    Added random moves for the player and monster

--- The_Fountain_of_Objects/Maelstrom.cs
+++ The_Fountain_of_Objects/Maelstrom.cs
@@ -8,19 +8,60 @@ namespace The_Fountain_of_Objects
 {
     public class Maelstrom
     {
-        internal static void MaelstromActions((int PlayerRow, int PlayerCol) playerTracker, string playerName)
+        internal static (int,int) MaelstromActionsPlayer((int PlayerRow, int PlayerCol) playerTracker, GameBoard<Game.RoomType> TagBoard, GameBoard<string> MoveBoard, Random DiceRoll, bool GameOver, Player GamePlayer)
         {
-            //Player Move
-            
-        }
-        /*internal static bool OutofBoundsCheck()
-        {
+            bool GoodTarget = false;
+            (int RowRng1, int RowRng2, int ColRng1, int ColRng2) Settings = SetBoardRnds(TagBoard);
+            {
 
+                    MoveBoard.Map[playerTracker.PlayerRow, playerTracker.PlayerCol] = null;
+                    playerTracker.PlayerRow = DiceRoll.Next(Settings.RowRng1, Settings.RowRng2);
+                    playerTracker.PlayerCol = DiceRoll.Next(Settings.ColRng1, Settings.ColRng2);
+                    MoveBoard.Map[playerTracker.PlayerRow, playerTracker.PlayerCol] = GamePlayer.PlayerName;
+                    
+
+                Game.RoomType CheckMap = TagBoard.Map[playerTracker.PlayerRow, playerTracker.PlayerCol];
+                    if (CheckMap == Game.RoomType.Entry) Console.WriteLine("Update with Entry message");
+                    else if (CheckMap == Game.RoomType.Fountain) Console.WriteLine("Update with Fountain Message");
+                    else if (CheckMap == Game.RoomType.Maelstrom) Console.WriteLine("Update with Maelstorm Message");
+                    else if (CheckMap == Game.RoomType.Amarok) Console.WriteLine("Update with Amarok Message");
+                    else if (CheckMap == Game.RoomType.Pit) Console.WriteLine("Update with Pit Message");
+                    else Console.WriteLine("Update with regular room message");
+                    return playerTracker;
+                
+                
+            }
         }
 
-        internal static bool CollisionCheck()
+        internal static void MaelstromActionsMoster((int PlayerRow, int PlayerCol) playerTracker, GameBoard<Game.RoomType> TagBoard, Random DiceRoll)
         {
-        
-        }*/
+            int SaveMaelstromRow = playerTracker.PlayerRow;
+            int SaveMaelstromCol = playerTracker.PlayerCol;
+            int MaelstromRow;
+            int MaelstromCol;
+            bool GoodTarget = false;
+
+            (int RowRng1, int RowRng2, int ColRng1, int ColRng2) Settings = SetBoardRnds(TagBoard);
+
+            {
+                do
+                {
+                    MaelstromRow = DiceRoll.Next(Settings.RowRng1, Settings.RowRng2);
+                    MaelstromCol = DiceRoll.Next(Settings.ColRng1, Settings.ColRng2);
+                    if (TagBoard.Map[MaelstromRow, MaelstromCol] == Game.RoomType.Regular) GoodTarget = true;
+                } while (GoodTarget == false);
+                TagBoard.Map[MaelstromRow, MaelstromCol] = Game.RoomType.Maelstrom;
+                TagBoard.Map[SaveMaelstromRow, SaveMaelstromCol] = Game.RoomType.Regular;
+            }
+        }
+
+        public static (int, int, int, int) SetBoardRnds(GameBoard<Game.RoomType> TagBoard)
+        {
+            (int row1, int row2, int col1, int col2) targetrnds;
+
+            if (TagBoard.Map.Length == 16) return targetrnds = (0, 3, 0, 3);
+            else if (TagBoard.Map.Length == 36) return targetrnds = (0, 5, 0, 5);
+            else /*(TagBoard.Map.Length == 64)*/ return targetrnds = (0, 7, 0, 7);
+        }
     }
 }
